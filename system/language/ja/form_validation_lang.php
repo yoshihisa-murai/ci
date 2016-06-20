c708ce79566c745b721c5c1a89e5ffd105ba2468
murai.yoshihisa <murai@planx.jp>
2016-06-20 16:07:44 +0900

    mod lang

--- system/language/ja/form_validation_lang.php
+++ system/language/ja/form_validation_lang.php
@@ -26,43 +26,43 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
- * @license	http://opensource.org/licenses/MIT	MIT License
- * @link	http://codeigniter.com
- * @since	Version 1.0.0
+ * @package    CodeIgniter
+ * @author    EllisLab Dev Team
+ * @copyright    Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright    Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license    http://opensource.org/licenses/MIT    MIT License
+ * @link    http://codeigniter.com
+ * @since    Version 1.0.0
  * @filesource
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['form_validation_required']				= '{field} は必須入力です。';
-$lang['form_validation_isset']					= 'The {field} field must have a value.';
-$lang['form_validation_valid_email']			= '{field} はメールアドレスを入力してください。';
-$lang['form_validation_valid_emails']			= 'メールアドレスが一致しません、。';
-$lang['form_validation_valid_url']				= 'そのメールアドレスは登録済みです。';
-$lang['form_validation_valid_ip']				= 'The {field} field must contain a valid IP.';
-$lang['form_validation_min_length']				= '{field} は {param} 文字以上で入力してください。';
-$lang['form_validation_max_length']				= '{field} は {param} 文字以下で入力してください。';
-$lang['form_validation_exact_length']			= 'The {field} field must be exactly {param} characters in length.';
-$lang['form_validation_alpha']					= '{field} はアルファベットのみ入力可能です。';
-$lang['form_validation_alpha_numeric']			= '{field} はアルファベットと数字のみ入力可能です。';
-$lang['form_validation_alpha_numeric_spaces']	= '{field} はアルファベットと数字とスペースが入力可能です。';
-$lang['form_validation_alpha_dash']				= 'The {field} field may only contain alpha-numeric characters, underscores, and dashes.';
-$lang['form_validation_numeric']				= '{field} は数字のみ入力可能です。';
-$lang['form_validation_is_numeric']				= 'The {field} field must contain only numeric characters.';
-$lang['form_validation_integer']				= '{field} は整数のみ入力可能です。';
-$lang['form_validation_regex_match']			= 'The {field} field is not in the correct format.';
-$lang['form_validation_matches']				= 'パスワードが一致しません。';
-$lang['form_validation_differs']				= 'The {field} field must differ from the {param} field.';
-$lang['form_validation_is_unique'] 				= 'その{field}は登録済みです。';
-$lang['form_validation_is_natural']				= 'The {field} field must only contain digits.';
-$lang['form_validation_is_natural_no_zero']		= 'The {field} field must only contain digits and must be greater than zero.';
-$lang['form_validation_decimal']				= 'The {field} field must contain a decimal number.';
-$lang['form_validation_less_than']				= 'The {field} field must contain a number less than {param}.';
-$lang['form_validation_less_than_equal_to']		= 'The {field} field must contain a number less than or equal to {param}.';
-$lang['form_validation_greater_than']			= 'The {field} field must contain a number greater than {param}.';
-$lang['form_validation_greater_than_equal_to']	= 'The {field} field must contain a number greater than or equal to {param}.';
-$lang['form_validation_error_message_not_set']	= 'Unable to access an error message corresponding to your field name {field}.';
-$lang['form_validation_in_list']				= 'The {field} field must be one of: {param}.';
+$lang['form_validation_required']               = '{field} は必須入力です。';
+$lang['form_validation_isset']                  = 'The {field} field must have a value.';
+$lang['form_validation_valid_email']            = '{field} はメールアドレスを入力してください。';
+$lang['form_validation_valid_emails']           = 'メールアドレスが一致しません、。';
+$lang['form_validation_valid_url']              = 'そのメールアドレスは登録済みです。';
+$lang['form_validation_valid_ip']               = 'The {field} field must contain a valid IP.';
+$lang['form_validation_min_length']             = '{field} は {param} 文字以上で入力してください。';
+$lang['form_validation_max_length']             = '{field} は {param} 文字以下で入力してください。';
+$lang['form_validation_exact_length']           = '{field} の文字数が正しくありません。';
+$lang['form_validation_alpha']                  = '{field} はアルファベットのみ入力可能です。';
+$lang['form_validation_alpha_numeric']          = '{field} はアルファベットと数字のみ入力可能です。';
+$lang['form_validation_alpha_numeric_spaces']   = '{field} はアルファベットと数字とスペースが入力可能です。';
+$lang['form_validation_alpha_dash']             = '{field} はアルファベットと数字とアンダースコア(_)、ハイフン(-)が入力可能です。';
+$lang['form_validation_numeric']                = '{field} は数字のみ入力可能です。';
+$lang['form_validation_is_numeric']             = '{field} は数字のみ入力可能です。';
+$lang['form_validation_integer']                = '{field} は整数のみ入力可能です。';
+$lang['form_validation_regex_match']            = 'The {field} field is not in the correct format.';
+$lang['form_validation_matches']                = 'パスワードが一致しません。';
+$lang['form_validation_differs']                = 'The {field} field must differ from the {param} field.';
+$lang['form_validation_is_unique']              = 'その{field}は登録済みです。';
+$lang['form_validation_is_natural']             = '{field} は0か、正の整数のみ入力可能です。';
+$lang['form_validation_is_natural_no_zero']     = '{field} は0以外の、正の整数のみ入力可能です。';
+$lang['form_validation_decimal']                = '{field} は少数のみ入力可能です。';
+$lang['form_validation_less_than']              = '{field} は {param} よりも小さい値を入力してください';
+$lang['form_validation_less_than_equal_to']     = '{field} は {param} 以下の値を入力してください。';
+$lang['form_validation_greater_than']           = '{field} は {param} よりも大きい値を入力してください。';
+$lang['form_validation_greater_than_equal_to']  = '{field} は {param} 以上の値を入力してください。';
+$lang['form_validation_error_message_not_set']  = 'Unable to access an error message corresponding to your field name {field}.';
+$lang['form_validation_in_list']                = '{field} は {param} から選択してください。';
