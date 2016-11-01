642c62d68a9cb63a8ab732f1c416024d2d621f1b
murai.yoshihisa <murai@planx.jp>
2016-11-01 12:00:15 +0900

    test

--- application/config/apiparams.php
+++ application/config/apiparams.php
@@ -7,3 +7,25 @@ $config['api_params']['meta'] = array(
 			'fallback_url' => 'http://localhost/work_projects/Nihtan_Vendor_Module/',
 			'receiver_url' => 'http://xxx.xxx.xxx.xxx/sample_receiver.php' // Point your domain or IP here then the path to the receiver php file
 			);
+
+if ( ENVIRONMENT !== 'production' ) {
+    $config['iwallet_api'] = array(
+        'cash_in_request_url' => 'https://test.iwl.world/api/MoneyRequest',
+        'cash_out_request_url' => 'https://test.iwl.world/en/settlement.php',
+        'user_name' => 'testHaciendaG082016',
+        'password' => '047c9MHWD0U',
+        'p_num' => '10028',
+        'from_account' => '63576856',
+        'bank_account' => '3469274',
+    );
+} else {
+    $config['iwallet_api'] = array(
+        'cash_in_request_url' => 'https://test.iwl.world/api/MoneyRequest',
+        'cash_out_request_url' => 'https://test.iwl.world/en/settlement.php',
+        'user_name' => 'testHaciendaG082016',
+        'password' => '047c9MHWD0U',
+        'p_num' => '10028',
+        'from_account' => '63576856',
+        'bank_account' => '3469274',
+    );
+}
